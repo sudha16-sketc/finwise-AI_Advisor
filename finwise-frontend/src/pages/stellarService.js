16234e7cc358f30381496ee70b9e48168fce8a4c
Sudhakar Sutar <sudhakarsutar101@gmail.com>
2026-04-02 12:49:51 +0530

    updated and fixed the transaction bar

--- finwise-frontend/src/pages/stellarService.js
+++ finwise-frontend/src/pages/stellarService.js
@@ -114,8 +114,6 @@ export const sendTransaction = async (sourcePublicKey, destinationAddress, amoun
 
     const response = await axios.post(`${BACKEND_API}/send`, {
       xdr: signed.signedTxXdr,
-      sender_address: sourcePublicKey,
-      amount: parseFloat(amount),
     });
 
     return response.data;
